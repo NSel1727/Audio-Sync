051861392eb8f583c04e47e4b908b9990f0ddd68
Anthony Castillo <acast698@fiu.edu>
2023-10-07 15:42:34 -0400

    Update 3, not passing in an API key, use default

--- Backend/api/getSong.py
+++ Backend/api/getSong.py
@@ -1,27 +1,18 @@
-from pydub import AudioSegment
+import asyncio
 from shazamio import Shazam
-import os
-
-
-# Shazam API credentials
-API_KEY = os.environ.get('SHAZAM_API_KEY')
-
-
+from pydub import AudioSegment
 
 async def identify_song(audio_file_path):
-    
     audio = AudioSegment.from_file(audio_file_path)
     audio = audio.set_channels(1).set_frame_rate(16000)
     
     intermediate_file = "../intermediate.wav"
     audio.export(intermediate_file, format="WAV")
 
-    shazam = Shazam(API_KEY)  # Using Shazam with its default settings
+    shazam = Shazam()  #Reverse, engineered, use default Shazam class
     
-
     try:
         song_info = await shazam.recognize_song(intermediate_file)
-
         if song_info:
             artist = song_info['track']['subtitle']
             title = song_info['track']['title']
@@ -34,15 +25,10 @@ async def identify_song(audio_file_path):
         print(f"Error occurred during song identification: {str(e)}")
         print('Song not identified.')
         return None
-    
-
 
 async def main():
     audio_file_path = 'output.wav'
-    
     await identify_song(audio_file_path)
 
 if __name__ == '__main__':
-    import asyncio
-
     asyncio.run(main())
