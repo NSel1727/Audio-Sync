c6c97f2ee3d409a4fb4d35ad03ebfe9238613680
NSel1727 <noahseligson18@gmail.com>
2023-10-07 14:14:54 -0400

    Design rest of simulation card and starting designing simulation

--- Frontend/src/Cards/simulation.js
+++ Frontend/src/Cards/simulation.js
@@ -1,24 +1,64 @@
-import { useState } from 'react';
+import { useState, useEffect} from 'react';
 import Logo from '../Components/Logo';
+import DOMPurify from 'dompurify'
 
-function Simulation(props) {
 
-  function onEnd(){
-    props.setIsRunning(false);
+function Simulation(props){
+
+  const [tableContent, setTableContent] = useState([]);
+  const [currentSong, setCurrentSong] = useState("Nothing is playing atm");
+  const [isGreen, setIsGreen] = useState(true);
+
+  function onButtonClick(){
+    if(isGreen){
+      setIsGreen(false);
+      fetch("/audio").then((res) =>
+            res.json().then((data) => {
+                console.log(data);
+            })
+        );
+    }else{
+      props.setIsRunning(false);
+    }
   }
 
   function leftPanel(){
-
+    return (
+      <div className='leftPanel'>
+        <Logo/>
+        <button type="button" class={(isGreen ? "green" : "") + " stopButton btn btn-danger"} onClick={() => onButtonClick()}>{isGreen ? "go" : "stop"}</button>
+      </div>
+    )
   }
 
   function rightPanel(){
-    
+    return (
+      <div className='rightPanel'>
+        <div className='currentSong'>
+          Current Song: {currentSong}
+        </div>
+        <div className='songTable'>
+          <table class="table table-hover">
+            <thead>
+              <tr>
+                <th>Song Name</th>
+                <th>Artist</th>
+              </tr>
+            </thead>
+            <tbody>
+              {tableContent}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    )
   }
 
   return (
     <div class="aligns-items-center justify-content-center card text-center w-50 position-absolute top-50 start-50 translate-middle">
       <div class="card-body">
-        
+        {leftPanel()}
+        {rightPanel()}
       </div>
     </div>
   );
