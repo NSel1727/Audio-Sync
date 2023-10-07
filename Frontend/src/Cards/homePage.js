eb5dcace6a419b39e6bda6a277e85ee203085188
Melanie Ehrlich <71104590+mel4ni3@users.noreply.github.com>
2023-10-07 14:05:55 -0400

    background

--- Frontend/src/Cards/homePage.js
+++ Frontend/src/Cards/homePage.js
@@ -8,7 +8,7 @@ function HomePage(props) {
   }
 
   return (
-      <div class="aligns-items-center justify-content-center card text-center w-50 position-absolute top-50 start-50 translate-middle">
+      <div class="homepage aligns-items-center justify-content-center card text-center w-50 position-absolute top-50 start-50 translate-middle">
         <div class="card-body">
           <div class="welcome">
             welcome to
