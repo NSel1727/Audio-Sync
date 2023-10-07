5f4d4ced5fdd68cd621ea34274e1d24d7ace7ed0
Anthony Castillo <acast698@fiu.edu>
2023-10-07 15:49:09 -0400

    Update 3, using default Shazam() class

--- Backend/api/getSong.py
+++ Backend/api/getSong.py
@@ -33,4 +33,5 @@ async def main():
 if __name__ == '__main__':
     asyncio.run(main())
 
-#update 3, after must change return types to something usable by frontend
+
+#update 3, after must change return types to something usable by frontend.
